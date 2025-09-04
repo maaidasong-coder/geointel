995deb1bcb6c77dea9f12039a0042b525cd8af69
maaidasong-coder <maaidasong@gmail.com>
2025-09-04 16:47:16 +0100

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 # src/models.py
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, func
 from .database import Base
 
 class Incident(Base):
@@ -9,3 +9,7 @@ class Incident(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True, nullable=False)
     location = Column(String, index=True, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"<Incident(id={self.id}, title={self.title}, location={self.location})>"
